67bc7b19d25e7e5eca55eb1bbf63dac9de7fa2d9
Jon Liebold <jliebold@outlook.com>
2016-05-14 22:35:43 -0400

    Created basic index.php with blog listing

--- builds/dev/index.php
+++ builds/dev/index.php
@@ -2,8 +2,7 @@
 <html lang="<?php language_attributes(); ?>">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Document</title>
 	<?php wp_head(); ?>
 </head>
@@ -16,11 +15,32 @@
 </header>
 
 <main role="main">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article class="blogPost">
+				<header class="postHeader">
+					<h2><?php the_title(); ?></h2>
+					<?php if ( get_the_modified_time() === get_the_time() ) : ?>
+						<p>Posted on <time datetime="<?php the_time( 'c' ); ?>"><?php the_date( 'l, F jS, Y' ); ?> at <?php the_time( 'g:ia T' ); ?></time></p>
+					<?php else : ?>
+						<p>Originally posted on <time datetime="<?php the_time( 'c' ); ?>"><?php the_date( 'l, F jS, Y' ); ?> at <?php the_time( 'g:ia T' ); ?></time></p>
+					<?php endif; ?>
+				</header>
+				<div class="postBody">
+					<?php the_excerpt(); ?>
+				</div>
+				<footer class="postFooter">
 
+				</footer>
+			</article>
+		<?php endwhile; ?>
+		<?php else :
+			esc_html__( 'Sorry, no posts matched your criteria.', 'jldc' );
+	endif; ?>
 </main>
 
 <footer id="pageFooter" role="contentinfo">
-
+	<!-- Footer Stuff will go here. Thinking Menu -->
 </footer>
 
 <?php wp_footer(); ?>
