e0fe0884cf0f7ca4234830e8583396cdec5e9a5c
Jon Liebold <jliebold@outlook.com>
2016-06-01 20:35:56 -0400

    Created script to load Adobe Typekit, enqueued it, and finished basic type variable declarations

--- builds/dev/functions.php
+++ builds/dev/functions.php
@@ -8,12 +8,13 @@ function jldc_enqueues() {
 	wp_enqueue_style( 'jldc-styles' );
 
 	// Register Scripts
-	wp_register_script( 'jldc-header-js', get_stylesheet_directory_uri() . '/_js/', null, null, false );
-	wp_register_script( 'jldc-script', get_stylesheet_directory_uri() . '/_js/', null, null, true );
+	wp_register_script( 'jldc-header-script', get_stylesheet_directory_uri() . '/_js/jldc-header.js', null, null, false );
+	wp_register_script( 'jldc-script', get_stylesheet_directory_uri() . '/_js/jldc-script.js', null, null, true );
 	wp_register_script( 'jldc-typekit-core', 'https://use.typekit.net/yih1lcp.js', null, null, false );
 
 	// Enqueue Scripts
 	wp_enqueue_script( 'jldc-typekit-core' );
+	wp_enqueue_script( 'jldc-header-script' );
 }
 
 add_action( 'wp_enqueue_scripts', 'jldc_enqueues' );
