3ad17ed253c0e0967b988e8c163238a32aca0c69
Jon Liebold <jliebold@outlook.com>
2016-06-04 19:29:00 -0400

    Added conditional heading element for masthead and corrected title element error and nav ID typo

--- builds/dev/header.php
+++ builds/dev/header.php
@@ -16,22 +16,29 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Document</title>
 	<?php wp_head(); ?>
 </head>
 <body>
 
 <div id="wrapper">
-
 	<header id="pageHeader" role="banner">
-		<h1 id="masthead">
-			<a href="<?php bloginfo( 'url' ); ?>">
-				<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/_img/logoMasthead.svg" alt="<?php echo bloginfo( 'name' ); ?>">
-			</a>
-		</h1>
+		<?php if ( is_singular() && ! is_front_page() ) : ?>
+			<h6 id="masthead">
+				<a href="<?php bloginfo( 'url' ); ?>">
+					<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/_img/logoMasthead.svg" alt="<?php echo bloginfo( 'name' ); ?>">
+				</a>
+			</h6>
+		<?php else : ?>
+			<h1 id="masthead">
+				<a href="<?php bloginfo( 'url' ); ?>">
+					<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/_img/logoMasthead.svg" alt="<?php echo bloginfo( 'name' ); ?>">
+				</a>
+			</h1>
+		<?php endif; ?>
+
 	</header>
 
-	<nav id="siteNave" role="navigation">
+	<nav id="siteNav" role="navigation">
 		<?php
 		wp_nav_menu( array(
 			'theme_location'    => 'main-menu',
